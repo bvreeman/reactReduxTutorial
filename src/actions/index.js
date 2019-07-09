76a6782c6f4d8b7942f69c86c9721a10c6bafb03
bvreeman <bvreeman@gmail.com>
2019-07-09 00:33:34 -0500

    updated the packages to get rid of vulnerability errors

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,13 @@ let user = {
     email: null
 }
 
+// export const simpleAction = () => dispatch => {
+//     dispatch({
+//      type: 'SIMPLE_ACTION',
+//      payload: 'result_of_simple_action'
+//     })
+//    }
+
 export default (state = user, action) => {
     switch (action.type) {
         case SIGNED_IN:
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,17 @@ let user = {
     email: null
 }
 
+// export default (state = {}, action) => {
+//     switch (action.type) {
+//      case 'SIMPLE_ACTION':
+//       return {
+//        result: action.payload
+//       }
+//      default:
+//       return state
+//     }
+//    }
+
 export default (state = user, action) => {
     switch (action.type) {
         case SIGNED_IN:
